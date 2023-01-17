6f691a4d142237dc1dc24a1d91dd896ef03cad2c
Trippington <122919435+Trippington@users.noreply.github.com>
2023-01-17 15:32:08 -0600

    Update script.js

--- script.js
+++ script.js
@@ -7,12 +7,54 @@ async function fetchData() {
         }
     };
     try {
-        const response = await fetch('https://weatherapi-com.p.rapidapi.com/forecast.json?q=Dallas&days=3&dt=2023-01-16', options);
+        //const response = await fetch('https://weatherapi-com.p.rapidapi.com/forecast.json?q=Dallas&days=3&dt=2023-01-16', options);
+        const response = await fetch('https://weatherapi-com.p.rapidapi.com/forecast.json?q=Dallas&days=3', options);
         const jsonResponse = await response.json();
 
-        console.log(jsonResponse);
+       
         myJavaScriptVar = JSON.stringify(jsonResponse);
+        tdata = JSON.parse(myJavaScriptVar);
+        temp_f = tdata.current.temp_f
+
+        const maxtemp_f_day0 = tdata.forecast.forecastday[0].day.maxtemp_f;
+        const maxtemp_f_day1 = tdata.forecast.forecastday[1].day.maxtemp_f;
+        const maxtemp_f_day2 = tdata.forecast.forecastday[2].day.maxtemp_f;
+
+        const mintemp_f_day0 = tdata.forecast.forecastday[0].day.mintemp_f;
+        const mintemp_f_day1 = tdata.forecast.forecastday[1].day.mintemp_f;
+        const mintemp_f_day2 = tdata.forecast.forecastday[2].day.mintemp_f;
+
+        //myJavaScriptVar = tdata.current.toString();
+
+        console.log(tdata);
+        console.log("tdata.current.temp_f is: " + tdata.current.temp_f);
+        // high temperatures
+        console.log("tdata.forecast is: " + tdata.forecast);
+        console.log("tdata.forecast.forecastday[0].day.maxtemp_f is: " + tdata.forecast.forecastday[0].day.maxtemp_f);
+        document.getElementById("1h").innerHTML = maxtemp_f_day0 + "&#176";
+
+        console.log("tdata.forecast.forecastday[1].day.maxtemp_f is: " + tdata.forecast.forecastday[1].day.maxtemp_f);
+        document.getElementById("2h").innerHTML = maxtemp_f_day1 + "&#176";;
+
+        console.log("tdata.forecast.forecastday[2].day.maxtemp_f is: " + tdata.forecast.forecastday[2].day.maxtemp_f);
+        document.getElementById("3h").innerHTML = maxtemp_f_day2 + "&#176";;
+
+        //low temperatures
+        console.log("tdata.forecast.forecastday[0].day.mintemp_f is: " + tdata.forecast.forecastday[0].day.mintemp_f);
+        document.getElementById("1l").innerHTML = mintemp_f_day0 + "&#176";;
+
+        console.log("tdata.forecast.forecastday[1].day.mintemp_f is: " + tdata.forecast.forecastday[1].day.mintemp_f);
+        document.getElementById("2l").innerHTML = mintemp_f_day1 + "&#176";;
+
+        console.log("tdata.forecast.forecastday[2].day.mintemp_f is: " + tdata.forecast.forecastday[2].day.mintemp_f);
+        document.getElementById("3l").innerHTML = mintemp_f_day2 + "&#176";;
+
+
+        //current temperatures
+        document.getElementById("current_temp").innerHTML = temp_f + "&#176";;
+        //all weather data
         document.getElementById("weather").innerHTML = myJavaScriptVar;
+
     } catch (error) {
         console.error(error);
     }
